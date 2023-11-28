811219c5be1129e30d1b97325ecb3d74c1e3d589
Mauro Fassi <maurofassi17@gmail.com>
2023-11-28 17:07:12 -0300

    creando clase test para metodo select

---
+++ src/main/java/test/TestManejoPersonas.java
@@ -0,0 +1,19 @@
+package test;
+
+import datos.PersonaDAO;
+import domain.Persona;
+import java.util.List;
+
+
+public class TestManejoPersonas {
+    public static void main(String[] args) {
+        PersonaDAO personaDao= new PersonaDAO();
+        List <Persona> personas=personaDao.seleccionar();
+        
+        for(Persona persona:personas){
+            System.out.println("persona = " + persona);
+        }
+        
+        
+    }
+}

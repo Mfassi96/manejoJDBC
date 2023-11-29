27c76ea6405c32a55600c9a87ebb85c3ea9ddf47
Mauro Fassi <maurofassi17@gmail.com>
2023-11-28 21:07:38 -0300

    Creando metodo insert

--- src/main/java/datos/PersonaDAO.java
+++ src/main/java/datos/PersonaDAO.java
@@ -5,44 +5,44 @@ import domain.Persona;
 import java.sql.*;
 import java.sql.ResultSet;
 import java.util.*;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PersonaDAO {
+
+    private static String SQL_SELECT = "SELECT * FROM persona";
+    private static String SQL_INSRT = "INSERT INTO persona(id_persona,nombre, apellido, email, telefono) VALUES (NULL,?,?,?,?)";
     
-    private static String SQL_SELECT="SELECT * FROM persona";
-    
-    public List <Persona> seleccionar(){
-        Connection conn=null;
-        PreparedStatement stmt= null;
-        ResultSet rs=null;
-        Persona persona=null;
-        List<Persona>personas=new ArrayList<>();
-        
-        
+    public List<Persona> seleccionar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Persona persona = null;
+        List<Persona> personas = new ArrayList<>();
+
         try {
-            conn=getConnection();
-            stmt=conn.prepareStatement(SQL_SELECT);
-            rs=stmt.executeQuery(); //devueve todo el set de resultado de la consulta
-            
-            while(rs.next()){
-                
-                int idPersona=rs.getInt("id_persona");
-                String nombre=rs.getString("nombre");
-                String apellido=rs.getString("apellido");
-                String telefono=rs.getString("telefono");
-                
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery(); //devueve todo el set de resultado de la consulta
+
+            while (rs.next()) {
+
+                int idPersona = rs.getInt("id_persona");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String telefono = rs.getString("telefono");
+
                 //se crea el objeto persona
-                persona=new Persona(idPersona, nombre, apellido, nombre, telefono);
-                
+                persona = new Persona(idPersona, nombre, apellido, nombre, telefono);
+
                 // y se agrega a la lista
                 personas.add(persona);
-                
-                
+
             }
-            
+
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
-        }finally{
+        } finally {
             try {
                 close(rs);
                 close(stmt);
@@ -50,11 +50,40 @@ public class PersonaDAO {
             } catch (SQLException ex) {
                 ex.printStackTrace(System.out);
             }
-            
-            
-           
+
         }
         return personas;
     }
-    
+
+    public int insertar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros=0;
+        
+        try {
+            conn=getConnection();
+            stmt=conn.prepareStatement("SQL_INSERT");
+            
+            //pasar valores a la consukta
+            stmt.setString(2, persona.getNombre());
+            stmt.setString(3, persona.getApellido());
+            stmt.setString(4, persona.getEmail());
+            stmt.setString(5, persona.getTelefono());
+            registros=stmt.executeUpdate(); //este metodo puede ejecutar sentencias tipo insert, update o delete
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }finally{
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+            
+        
+    }
+        return registros;
+
+    }
+
 }
